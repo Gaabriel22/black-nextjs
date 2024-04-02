e6ff79ea036173ce88ab9ad05731c80881033945
Gabriel Amaral <gabrielvieira2205@gmail.com>
2024-04-02 09:17:43 -0300

    Aplicação Finalizada

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -1,9 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next"
 import products from '../../../../database.json'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { id } = req.query
+    const { id } = req.query;
 
     const product = products.find(prod => prod.id === Number(id))
+    if (!product) {
+        return res.status(404).json({ message: 'Product not found' })
+    }
+
     res.status(200).json(product)
-}   
+}
